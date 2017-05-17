701a2cd4ef0b02d63b9d078066d4d04678aefaf6
SefikaBoz <sefikaboz12@gmail.com>
2017-05-17 21:37:42 +0300

    Create ChangeRequest.cs

---
+++ ChangeRequest.cs
@@ -0,0 +1,30 @@
+# 17-05-2017---3
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMSDN
+{
+    class ChangeRequest : WorkItem
+    {
+        protected int OriginalItemId { get; set; }
+        public ChangeRequest()
+        {
+            //this.ID = 45;
+            //this.Title = "Change Request twest title";
+            ////Console.Write(ToString());
+        }
+        public ChangeRequest(string title, string desc, TimeSpan joblen, int originalId)
+        {
+            this.ID = GetNextId();
+            this.Title = title;
+            this.Description = desc;
+            this.jobLength = jobLength;
+            this.OriginalItemId = originalId;
+            
+        }
+    }
+}
